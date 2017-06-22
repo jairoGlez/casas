592b773d9dd8abb0c1ff43d8c7038c8d4e027393
Jairo Gonzalez <jairo.glez@hotmail.com>
2017-06-21 22:36:27 -0500

    Menu agregado

--- casas.py
+++ casas.py
@@ -1,18 +1,19 @@
 import sqlite3
 
-db = sqlite3.connect("casas.db")
-c = db.cursor()
+class Casa:
+    db = sqlite3.connect("casas.db")
+    c = db.cursor()
 
-def mostrarCompradores():
-    r = c.execute("SELECT * FROM comprador")
-    r = r.fetchall()
+    def mostrarCompradores(self):
+        r = self.c.execute("SELECT * FROM comprador")
+        r = r.fetchall()
+        lista = []
 
-    for fila in r:
-        print (fila)
+        for fila in r:
+            lista.append(fila)
 
-def nuevoComprador(datos):
-    c.execute("INSERT INTO comprador (email,nombre,apellido1,apellido2,idVendedor) VALUES (?,?,?,?,?)",(datos[0],datos[1],datos[2],datos[3],datos[4]))
-    db.commit()
+        return lista
 
-nuevoComprador(["toño78@hotmail.com","antonio","jimenez","torrez",5])
-mostrarCompradores()
+    def nuevoComprador(self,datos):
+        self.c.execute("INSERT INTO comprador (email,nombre,apellido1,apellido2,idVendedor) VALUES (?,?,?,?,?)",(datos[0],datos[1],datos[2],datos[3],datos[4]))
+        self.db.commit()
